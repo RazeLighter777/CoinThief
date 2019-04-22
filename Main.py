db1dbcfd34264fc85fa01120b62590a8be42283c
justin <jtsuess19146@marmionacademy.org>
2019-04-21 20:22:22 -0500

    baclava

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import random
 guysName = "Daniel"
 print("hello? Have we been lost in space?")
 print("Anybody? This is " + guysName)
@@ -34,11 +35,32 @@ time.sleep(1)
 print ("Go!")
 time.sleep(1)
 enemiesname = "Globber"
-Globberhealth = 10
 Globberdamage = 1
 playerhealth = 30
 enemiesleft = 3
-while enemiesleft > 0:
-     print ("You've come against a " + enemiesname + "!") 
-     enemiesleft = enemiesleft - 1  
+while enemiesleft > 0:    
+    print ("You've come against a " + enemiesname + "!") 
+    Globberhealth = 10
+    while Globberhealth > 0 and playerhealth > 0:
+        waitTime = random.randint(1,3)
+        time.sleep(waitTime)
+        startTime = int(round(time.time() * 1000))
+        attackletter = input("type attack letter: ")
+        endTime = int(round(time.time() * 1000))
+        elapsedTime = endTime - startTime
+        if elapsedTime > 1000:
+            playerhealth = playerhealth - Globberdamage
+            print ("Ouch!")
+            continue
+        if attackletter == "E":
+            Globberhealth = Globberhealth - 8
+            print ("Globber: Oof.") 
+        if attackletter == "F":
+            Globberhealth = Globberhealth - attack
+            print ("Globber: Oof.") 
+        print("Globber heath" + str(Globberhealth))
+    if playerhealth < 0:
+        print ("*Dead*")
+        exit()        
+    enemiesleft = enemiesleft - 1  
 print ("Good Job!")
